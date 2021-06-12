13588d35b9444ea7b7a307380a2277aa57707eb8
Wilson <wilsontu4979@gmail.com>
2021-06-12 11:22:23 -0700

    updated tokenizer (lexer)

--- lexer.cpp
+++ lexer.cpp
@@ -7,26 +7,23 @@ char separators[] = {'(', ')', '[', ']', '{', '}', ',', ':', ';'};
 char operators[] = {'*', '+', '-', '=', '/', '>', '<', '%'};
 string keywords[13] = {"int", "float", "bool", "true", "false", "if", "else", "while", "put", "get", "begin", "end", "endif"};
 
-
-
 int tokenizer(char currChar, int currState);
 
 int DFSM[4][4] = {
-  /*
+    /*
     STATE 0 is an accepting state
     l = letters
     d = digits
     s = seperator/operator
 
   */
-  // l, d, s
+    // l, d, s
     {1, 2, 3, 0},
     {1, 2, 3, 0}
-    
+
 };
 
-
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
     ifstream inFile;
     ofstream outFile;
@@ -61,25 +58,65 @@ int tokenizer(char currChar, int currState)
 
     int currentState = currState;
 
-    // If character is a alpha, check first column of states
     if (isalpha(currChar))
     {
+        cout << "Alpha" << endl;
+        cout << "Current State: " << currentState << endl;
         currentState = DFSM[currentState][0];
+        cout << "New State: " << currentState << endl;
         return currentState;
     }
     // If character is a digit, check second column of states
-    else if (isdigit(currChar)){
+    else if (isdigit(currChar))
+    {
+        cout << "Digit" << endl;
+        cout << "Current State: " << currentState << endl;
         currentState = DFSM[currentState][1];
+        cout << "New State: " << currentState << endl;
+        return currentState;
+    }
+    else if (isspace(currChar))
+    {
+        cout << "Space" << endl;
+        cout << "Current State: " << currentState << endl;
+        currentState = DFSM[currentState][0];
+        cout << "New State: " << currentState << endl;
+        return currentState;
+    }
+    else
+    {
+        cout << "Others" << endl;
+        for (int i = 0; i < 8; i++)
+        {
+            if (currChar == separators[i])
+            {
+                cout << "Seperator" << endl;
+                cout << "Current State: " << currentState << endl;
+                currentState = DFSM[currentState][4];
+                cout << "New State: " << currentState << endl;
+                return currentState;
+            }
+        }
+        for (int i = 0; i < 7; i++)
+        {
+            if (currChar == operators[i])
+            {
+                cout << "Operator" << endl;
+                cout << "Current State: " << currentState << endl;
+                currentState = DFSM[currentState][4];
+                cout << "New State: " << currentState << endl;
+                return currentState;
+            }
+        }
     }
 
-    
     return 0;
 }
 
-bool isalpha(char ch) {
-
+bool isalpha(char ch)
+{
 }
 
-bool isdigit(char ch) {
-
+bool isdigit(char ch)
+{
 }
