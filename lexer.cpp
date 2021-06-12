7f88ab224c3b4012b6b25d285456f654bfc2ae39
Vincent Nguyen <43223281+NguyenVincentxlll3@users.noreply.github.com>
2021-06-12 15:15:16 -0700

    Update lexer.cpp
    Added a new print function. DFSM updated by Yuichu.

--- lexer.cpp
+++ lexer.cpp
@@ -1,37 +1,73 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 using namespace std;
 
-char separators[] = {'(', ')', '[', ']', '{', '}', ',', ':', ';'};
-char operators[] = {'*', '+', '-', '=', '/', '>', '<', '%'};
-string keywords[13] = {"int", "float", "bool", "true", "false", "if", "else", "while", "put", "get", "begin", "end", "endif"};
+char separators[] = { '(', ')', '[', ']', '{', '}', ',', ':', ';' };
+char operators[] = { '*', '+', '-', '=', '/', '>', '<', '%' };
+string keywords[13] = { "int", "float", "bool", "true", "false", "if", "else", "while", "put", "get", "begin", "end", "endif" };
+
+vector<char> buffer;
+ifstream inFile;
+ofstream outFile;
 
 int tokenizer(char currChar, int currState);
+void print();
 
-int DFSM[4][4] = {
-    /*
-    STATE 0 is an accepting state
-    l = letters
-    d = digits
-    s = seperator/operator
+// Record Class
+class record
+{
+private:
+    // Contains token value and lexeme
+    string token, lexeme;
 
-  */
-    // l, d, s
-    {1, 2, 3, 0},
-    {1, 2, 3, 0}
+public:
+    string getToken()
+    {
+        return this->token;
+    }
 
+    string getLexeme()
+    {
+        return this->lexeme;
+    }
+
+    void setToken(string s)
+    {
+        this->token = s;
+    }
+
+    void setLexeme(string s)
+    {
+        this->lexeme = s;
+    }
 };
 
-int main(int argc, char *argv[])
+
+int DFSM[6][3] = {
+    /*
+    l = letters
+    d = digits
+    f = not letter or digit
+    */
+
+    //                        l, d, f
+    /* Starting   State 0: */{1, 4, 5}, // not accepted, need at least one l or d input.
+    /* Identifier State 1: */{2, 3, 5}, // accepted
+    /* Id is l,   State 2: */{2, 3, 5}, // accepted
+    /* Id is d,   State 3: */{2, 3, 5}, // accepted
+    /* Integer    State 4: */{5, 4, 5}, // accepted
+    /* End,       State 5: */{5, 5, 5}  // not accepted
+};
+
+int main(int argc, char* argv[])
 {
-    ifstream inFile;
-    ofstream outFile;
     char character;
     int currentState = 0;
 
     inFile.open(argv[1]);
-    outFile.open(argv[2]);
+    outFile.open("Tokens.txt");
 
     //checks if file opens correctly
     if (!inFile)
@@ -40,10 +76,11 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    while (!inFile.eof())
+    while (inFile.get(character))
     {
-        character = inFile.get();
         currentState = tokenizer(character, currentState);
+        if (currentState == 5)
+            currentState = 0;
     }
 
     inFile.close();
@@ -55,7 +92,6 @@ int main(int argc, char *argv[])
 // Tokenizer returns the next state
 int tokenizer(char currChar, int currState)
 {
-
     int currentState = currState;
 
     if (isalpha(currChar))
@@ -64,6 +100,7 @@ int tokenizer(char currChar, int currState)
         cout << "Current State: " << currentState << endl;
         currentState = DFSM[currentState][0];
         cout << "New State: " << currentState << endl;
+        buffer.push_back(currChar);
         return currentState;
     }
     // If character is a digit, check second column of states
@@ -73,14 +110,17 @@ int tokenizer(char currChar, int currState)
         cout << "Current State: " << currentState << endl;
         currentState = DFSM[currentState][1];
         cout << "New State: " << currentState << endl;
+        buffer.push_back(currChar);
         return currentState;
     }
     else if (isspace(currChar))
     {
         cout << "Space" << endl;
         cout << "Current State: " << currentState << endl;
-        currentState = DFSM[currentState][0];
+        currentState = DFSM[currentState][2];
         cout << "New State: " << currentState << endl;
+        if (buffer.size() != 0)
+            print();
         return currentState;
     }
     else
@@ -92,8 +132,10 @@ int tokenizer(char currChar, int currState)
             {
                 cout << "Seperator" << endl;
                 cout << "Current State: " << currentState << endl;
-                currentState = DFSM[currentState][4];
+                currentState = DFSM[currentState][2];
                 cout << "New State: " << currentState << endl;
+                if (buffer.size() != 0)
+                    print();
                 return currentState;
             }
         }
@@ -103,8 +145,10 @@ int tokenizer(char currChar, int currState)
             {
                 cout << "Operator" << endl;
                 cout << "Current State: " << currentState << endl;
-                currentState = DFSM[currentState][4];
+                currentState = DFSM[currentState][2];
                 cout << "New State: " << currentState << endl;
+                if (buffer.size() != 0)
+                    print();
                 return currentState;
             }
         }
@@ -113,10 +157,12 @@ int tokenizer(char currChar, int currState)
     return 0;
 }
 
-bool isalpha(char ch)
-{
-}
-
-bool isdigit(char ch)
-{
+void print() {
+    //prints the entire buffer and writes it to token.txt
+    for (vector<char>::iterator it = buffer.begin(); it != buffer.end(); ++it) {
+        cout << *it;
+        outFile << *it;
+    }
+    outFile << endl;
+    buffer.clear();
 }
